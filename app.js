46a5ca7407d347b635ff5b6e8b8a1dca645c1bf8
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-04 00:19:27 +0000

    Added nodemon

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next){
+    console.log('First piece of middleware!');
+    next();
+});
+
+app.use(function(req, res, next){
+    console.log('Second piece of middleware!');
+    next();
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, function(){
-    console.log()
+    console.log('Express server is listening on port: ' + port);
 });
