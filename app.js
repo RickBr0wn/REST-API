3c7fda6577c8eed9f613a5f30a87c2e87831b415
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-04 00:32:50 +0000

    Added 'body-parser'

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const jsonParser = require('body-parser').json;
 
 app.use(function(req, res, next){
     console.log('The leaves on the trees are', req.query.color);
