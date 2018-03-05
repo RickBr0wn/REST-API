7b1915c74eb51475f8b6cd57d2a0b403bce356b2
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-05 14:39:07 +0000

    Added sort function

--- models.js
+++ models.js
@@ -3,6 +3,17 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
+
+var sortAnswers = function(a, b){
+    // - negative a before b
+    // 0 no change
+    // + positive a after b
+    if(a.votes === b.votes){
+        return b.updatedAt - a.updatedAt;
+    }
+    return b.votes - a.votes;
+};
+
 var AnswerSchema = new Schema({
     text: String,
     createdAt: {type: Date, default: Date.now},
@@ -14,6 +25,11 @@ var QuestionSchema = new Schema({
     text: String,
     createdAt: {type: Date, default: Date.now},
     answers: [AnswerSchema]
+});
+
+QuestionSchema.pre("save", function(next){
+    this.answers.sort(sortAnswers);
+    next();
 });
 
 var Question = mongoose.model("Question", QuestionSchema);
